b5ad35405a643aed9fb325efabb9a8ce88e52dfd
jugelington <julianbeightondykes@gmail.com>
2018-10-26 12:38:50 +0100

    DB_URL now defined by ONLY MONGODB_URI

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { DB_URL } = process.env.MONGODB_URI || require('./config');
+const DB_URL = process.env.MONGODB_URI;
 const express = require('express');
 const app = express();
 
