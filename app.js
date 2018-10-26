18e0222f0a6f729f561e9493e7244450e190ce67
jugelington <julianbeightondykes@gmail.com>
2018-10-26 15:31:21 +0100

    slight reconfigure

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB_URL = process.env.MONGODB_URI;
+const { DB_URL } = require('./config') || process.env.MONGODB_URI;
 const express = require('express');
 const app = express();
 
