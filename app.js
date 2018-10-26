3f7312aac7b1a2dbc30eb28edea799c70f7c5352
jugelington <julianbeightondykes@gmail.com>
2018-10-26 12:36:13 +0100

    maybe working?

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { DB_URL } = process.env.DB_URL || require('./config');
+const { DB_URL } = process.env.MONGODB_URI || require('./config');
 const express = require('express');
 const app = express();
 
