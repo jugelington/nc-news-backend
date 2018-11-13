7a05309bdea15b140185130db692122513436bf5
jugelington <julianbeightondykes@gmail.com>
2018-11-13 15:19:19 +0000

    attempting refactoring of getUserByUsername

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,6 @@ const { User } = require('../models');
 exports.getUserByUsername = (req, res, next) => {
   const { username } = req.params;
   User.find({ username: username })
-    .then(user => res.send({ user }))
+    .then(user => res.send({ ...user }))
     .catch(next);
 };
