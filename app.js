2020e956d7afe6e0c68d808e74195b0a33324483
jugelington <julianbeightondykes@gmail.com>
2018-11-14 10:10:12 +0000

    tweaking cors

--- app.js
+++ app.js
@@ -13,15 +13,6 @@ app.use(bodyParser.json());
 app.use(cors());
 app.set('view engine', 'ejs');
 
-// app.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   next();
-// });
-
 mongoose
   .connect(
     DB_URL,
