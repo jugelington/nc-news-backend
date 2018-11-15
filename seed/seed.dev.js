f6c6d5c7ea717f16e12f2fc93ebb65cd1f03bfb1
jugelington <julianbeightondykes@gmail.com>
2018-11-15 22:29:56 +0000

    tweaked seeding; changed user avatar images

--- seed/seed.dev.js
+++ seed/seed.dev.js
@@ -1,7 +1,6 @@
-process.env.NODE_ENV = 'production';
 const seedDB = require('./seed');
 const mongoose = require('mongoose');
-const { DB_URL } = require('../config');
+const DB_URL = require('../config');
 const { articles, comments, topics, users } = require('./devData');
 
 mongoose
